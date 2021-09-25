e4b671c720d45fe48f65fd2d4fac1f1d5ace6153
Cameron Savage <csavage5@me.com>
2021-09-24 18:05:17 -0700

    re-organized client code to prepare for tunnel communication
    - started tunnel server-side code

--- client.c
+++ client.c
@@ -21,72 +21,83 @@ struct message{
 
 static char *addrTunnel;
 static char *portTunnel;
-static char *addrRemote;
-static char *portRemote;
-
+static char *addrServer;
+static char *portServer;
 
 struct addrinfo hints;
-struct addrinfo* pRemoteAddrInfo;
+struct addrinfo* pServerAddrInfo;
+int  sockfd, n;
+char recvline[MAXLINE + 1];
 
 char tempBuffer[MAXLINE];
 
 //struct sockaddr_in servaddr;
 
-void decodemsg(char *msg, int n);
-void displayMessage();
+void ConnectToServer();
+void WaitForServerResponse();
+
+void DecodeMsg(char *msg, int n);
+
+void DisplayServerInfo();
+void DisplayMsgStruct();
 
 int main(int argc, char **argv) {
-    int     sockfd, n;
-    char    recvline[MAXLINE + 1];
-    
-
 
     if (argc <= 1 || argc == 4 || argc >= 5) {
         printf("usage: client [ <tunnel IP address | hostname> <tunnel port> ] <server IP address | hostname> <server port>\n");
         exit(1);
     } else if (argc == 3) {
-        addrRemote = argv[1];
-        portRemote = argv[2];   
+        addrServer = argv[1];
+        portServer = argv[2]; 
+        
+        ConnectToServer();
+        WaitForServerResponse();
+
+        DisplayServerInfo();
+        DisplayMsgStruct();
+
     } else if (argc == 5) {
         addrTunnel = argv[1];
         portTunnel = argv[2];
-        addrRemote = argv[3];
-        portRemote = argv[4]; 
+        addrServer = argv[3];
+        portServer = argv[4]; 
+
+        // sendToTunnel()
+        //  - get ack
+        //  - send server IP and name
+        // waitForServerResponse()
+        //  - decodemsg()
     }
     
-    
+
+    exit(0);
+}
+
+void ConnectToServer() {
     bzero(&hints, sizeof(hints));
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
     //hints.ai_protocol = MSGLEN
     //hints.ai_flags = 
 
-    int error = getaddrinfo(addrRemote, portRemote, &hints, &pRemoteAddrInfo);
+    int error = getaddrinfo(addrServer, portServer, &hints, &pServerAddrInfo);
     if (error == 1) {
         printf("Error retrieving remote address info\n");
         exit(1);
     }
-
 
     if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("socket error\n");
         exit(1);
     }
 
-    // not needed, getaddrinfo does this and saves it in pRemoteAddrInfo
-    // bzero(&servaddr, sizeof(servaddr));
-    // servaddr.sin_family = AF_INET;
-    // servaddr.sin_port = htons(DAYTIME_PORT);  /* daytime server */
-    // if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
-    //     printf("inet_pton error for %s\n", argv[1]);
-    //     exit(1);
-    // }
-
-    if (connect(sockfd, pRemoteAddrInfo->ai_addr, pRemoteAddrInfo->ai_addrlen) < 0) {
+    if (connect(sockfd, pServerAddrInfo->ai_addr, pServerAddrInfo->ai_addrlen) < 0) {
         printf("connect error\n");
         exit(1);
     }
+}
 
+void WaitForServerResponse() {
     while ( (n = read(sockfd, recvline, MAXLINE)) > 0) {
         recvline[n] = 0;        /* null terminate */
         // if (fputs(recvline, stdout) == EOF) {
@@ -102,13 +113,11 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    decodemsg(recvline, n);
-
-    exit(0);
+    DecodeMsg(recvline, n);
 }
 
-void decodemsg(char message[], int n) {
-    printf("Decoding message...\n");
+void DecodeMsg(char message[], int n) {
+    printf("Decoding message...\n\n");
     //printf("Raw message:\n%s\n", message);
     
     bzero(&msg, sizeof(msg));
@@ -144,18 +153,18 @@ void decodemsg(char message[], int n) {
         strncpy(msg.payload, pToken + msg.addrlen + msg.timelen, msg.msglen);
     }
 
-    displayMessage();
-
 }
 
-void displayMessage() {
-    
-    getnameinfo(pRemoteAddrInfo->ai_addr, pRemoteAddrInfo->ai_addrlen, tempBuffer, MAXLINE, NULL, 0, 0);
+void DisplayServerInfo() {
+    getnameinfo(pServerAddrInfo->ai_addr, pServerAddrInfo->ai_addrlen, tempBuffer, MAXLINE, NULL, 0, 0);
     printf("Server name: %s\n", tempBuffer);
     bzero(&tempBuffer, MAXLINE);
 
-    inet_ntop(AF_INET, &((struct sockaddr_in *) pRemoteAddrInfo->ai_addr)->sin_addr.s_addr, tempBuffer, pRemoteAddrInfo->ai_addrlen);
+    inet_ntop(AF_INET, &((struct sockaddr_in *) pServerAddrInfo->ai_addr)->sin_addr.s_addr, tempBuffer, pServerAddrInfo->ai_addrlen);
     printf("IP Address: %s\n", tempBuffer); 
+}
+
+void DisplayMsgStruct() {
     
     printf("Time: %s", msg.currtime);
     printf("Who:\n%s", msg.payload);
--- tunnel.c
+++ tunnel.c
@@ -1,15 +1,60 @@
+#include <netinet/in.h>
+#include <time.h>
+#include <strings.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
 
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
-//TODO 
-// accept input from client
+#define MAXLINE 4096
+#define LISTENQ 1024
 
+struct message{
+    int addrlen, timelen, msglen;
+    char addr[MAXLINE];
+    char currtime[MAXLINE];
+    char payload[MAXLINE];
+}msg;
+
+// Server
+int listenfd, connfd;
+struct sockaddr_in servaddr;
+int daytimePort;
+
+struct sockaddr_in clientAddr;
+socklen_t clientAddrLen = sizeof(clientAddr);
+
+// client
 
 
 
 int main(int argc, char **argv) {
+    // get port
+
+    if (argc != 2) {
+        printf("usage: server <server port>\n");
+        exit(1);
+    } 
+    
+    if ( (daytimePort = atoi(argv[1])) == 0) {
+        printf("Error: either did not input a number for <server port> or entered invalid port 0\n");
+        printf("usage: server <server port>\n");
+    }
 
 
-    // wait for client
+    for ( ; ; ) {
+        
+        // wait for client
+        waitForClient();
+
+
+    }
+    
 
     // get server ip / port from client
 
@@ -21,4 +66,27 @@ int main(int argc, char **argv) {
 
     // forward message to client
         // print forward info
+}
+
+void waitForClient() {
+
+
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(daytimePort); /* daytime server */
+
+    bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+    listen(listenfd, LISTENQ);
+    printf("Listening...\n\n");
+
+    connfd = accept(listenfd, (struct sockaddr *) &clientAddr, &clientAddrLen);
+}
+
+void sendMessageToClient() {
+    char outgoingBuffer[MAXLINE];
+    write(connfd, outgoingBuffer, strlen(outgoingBuffer));
+    close(connfd);
 }
